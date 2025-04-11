8e837eb3adefd9d65a5a923fcb6929aa1857544e
Tamara Sibileva <sibilevato@gmail.com>
2025-04-11 13:48:22 +1200

    Changed TemplateSteps.java

--- src/test/java/ru/netology/web/steps/TemplateSteps.java
+++ src/test/java/ru/netology/web/steps/TemplateSteps.java
@@ -1,13 +1,19 @@
 package ru.netology.web.steps;
 
 import com.codeborne.selenide.Selenide;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Пусть;
 import io.cucumber.java.ru.Тогда;
+import org.junit.jupiter.api.BeforeEach;
+import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashBoardPage;
 import ru.netology.web.page.LoginPage;
 import ru.netology.web.page.TransferPage;
 import ru.netology.web.page.VerificationPage;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static ru.netology.web.data.DataHelper.*;
@@ -17,6 +23,8 @@ public class TemplateSteps {
     private static VerificationPage verificationPage;
     private static DashBoardPage dashBoardPage;
     private static TransferPage transferPage;
+    CardInfo cardFrom;
+    CardInfo cardTo;
 
     @Пусть("пользователь залогинен с именем {string} и паролем {string}")
     public void loginWithNameAndPassword(String login, String password) {
@@ -26,24 +34,33 @@ public class TemplateSteps {
         dashBoardPage.verifyDashBoardPage();
     }
 
-    @Когда("пользователь переводит {string} рублей с карты с номером 5559_0000_0000_0002 на свою 1 карту с главной страницы")
-    public void transferFromSecondCardToFirstCard(String amount) {
-        transferPage = dashBoardPage.selectCardToTransfer(getFirstCardInfo());
-        dashBoardPage = transferPage.makeValidTransfer(amount, getSecondCardInfo());
+    @Когда("пользователь переводит {string} рублей с карты с номером {string} на свою карту с номером {string} с главной страницы")
+    public void transferFromCardToCard(String amount, String from, String to) {
+        if (from.equals(getFirstCardInfo().getCardNumber())) {
+            cardFrom = getFirstCardInfo();
+        } else {
+            cardFrom = getSecondCardInfo();
+        }
+
+        if (to.equals(getFirstCardInfo().getCardNumber())) {
+            cardTo = getFirstCardInfo();
+        } else {
+            cardTo = getSecondCardInfo();
+        }
+
+        transferPage = dashBoardPage.selectCardToTransfer(cardTo);
+        dashBoardPage = transferPage.makeValidTransfer(String.valueOf(amount), cardFrom);
         dashBoardPage.reloadDashBoardPage();
     }
 
-    @Тогда("баланс его 1 карты из списка на главной странице должен стать {string} рублей")
-    public void checkFirstCardBalance(String expectedBalance) {
-        assertAll(() -> dashBoardPage.checkCardBalance(getFirstCardInfo(), expectedBalance));
-    }
+    @Тогда("баланс его карты с номером {string} должен стать {string} рублей")
+    public void checkCardToBalance(String to, String expectedBalance) {
+        if (to.equals(getFirstCardInfo().getCardNumber())) {
+            cardTo = getFirstCardInfo();
+        } else {
+            cardTo = getSecondCardInfo();
+        }
 
-//    var amount = generateValidAmount(secondCardBalance);
-//    var expectedFirstCardBalance = firstCardBalance + amount;
-//    var expectedSecondCardBalance = secondCardBalance - amount;
-//    var transferPage = dashBoardPage.selectCardToTransfer(firstCardInfo);
-//    dashBoardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
-//        dashBoardPage.reloadDashBoardPage();
-//    assertAll(() -> dashBoardPage.checkCardBalance(firstCardInfo, expectedFirstCardBalance),
-//            () -> dashBoardPage.checkCardBalance(secondCardInfo, expectedSecondCardBalance));
+        assertAll(() -> dashBoardPage.checkCardBalance(cardTo, expectedBalance));
+    }
 }
